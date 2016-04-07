ec0387990e00f7b03371d53cd32a6eec46185de4
Molly Curtin and Oskar Radon <oskarradonkimball@gmail.com>
2016-04-07 14:59:47 -0700

    add terrazzo background to application template

--- app/helpers/comma-adder.js
+++ app/helpers/comma-adder.js
@@ -1,10 +1,8 @@
 import Ember from 'ember';
 
 export function commaAdder(params) {
-
-    var price = params[0];
-      console.log(price);
-      return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
+  var price = params[0];
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 export default Ember.Helper.helper(commaAdder);
